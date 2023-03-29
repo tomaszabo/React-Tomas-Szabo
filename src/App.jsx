b4a8f96aa3f188ace4eeabced9f4f29d84417e4d
Tomas Szabo <tomas_95@live.com.ar>
2023-03-28 21:47:59 -0300

    entrega

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nabvarq from "./compounets/Nabvar/navbar";
+
+function App() {
+	return (
+		<div>
+			<Nabvarq />
+      
+			<h1>Hola mundo</h1>
+		</div>
+	);
+}
+
+export default App;
